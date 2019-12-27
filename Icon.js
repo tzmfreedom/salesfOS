66ccb9cf0c3f540a6773a6bad287bab348acb08a
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-12-27 23:47:05 +0900

    Updates

--- Icon.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-var Icon = function (props) {
-    return (React.createElement("div", { id: props.id, className: "app-icon game" },
-        React.createElement("div", { className: "relative" },
-            React.createElement("img", { src: props.img }),
-            React.createElement("span", { className: "app-icon-name" }, props.name))));
-};
-export default Icon;
-//# sourceMappingURL=Icon.js.map
