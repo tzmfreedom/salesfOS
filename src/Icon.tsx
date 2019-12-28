6ed9da687f5ad394143215dd5b842cefe73d1bb1
tzmfreedom <makoto_tajitsu@hotmail.co.jp>
2019-12-28 20:57:24 +0900

    update

--- src/Icon.tsx
+++ src/Icon.tsx
@@ -1,16 +1,53 @@
-import React, { useReducer, useCallback, useEffect } from 'react';
+import React, { useReducer, useCallback, useEffect, CSSProperties } from 'react';
 
 interface IconProperty {
   id: string
   img: any 
   name: string
-  style: any
+  style: CSSProperties
   left: number
   top: number
-  onDoubleClick: any
+  onDoubleClick: (e: React.MouseEvent) => void
 }
 
-const reducer = (state: any, action: any) => {
+interface State {
+  x: number
+  y: number
+  top: number
+  left: number
+  selected: boolean
+  dragging: boolean
+}
+
+type Action = (
+  | ReturnType<typeof dragStart>
+  | ReturnType<typeof drag>
+  | ReturnType<typeof mouseup>
+)
+
+const dragStart = (x: number, y: number) => {
+  return {
+    type: 'drag_start' as const,
+    x,
+    y,
+  }
+}
+
+const drag = (x: number, y: number) => {
+  return {
+    type: 'drag' as const,
+    x,
+    y,
+  }
+}
+
+const mouseup = () => {
+  return {
+    type: 'mouseup' as const,
+  }
+}
+
+const reducer = (state: State, action: Action) => {
   switch (action.type) {
     case 'drag_start':
       return {
@@ -19,9 +56,8 @@ const reducer = (state: any, action: any) => {
         y: action.y,
         selected: true,
       }
-    case 'drag_dialog':
+    case 'drag':
       if (!state.selected) return state;
-
       return {
         ...state,
         x: action.x,
@@ -31,7 +67,6 @@ const reducer = (state: any, action: any) => {
       }
     case 'mouseup':
       if (!state.selected) return state;
-      
       return {
         ...state,
         selected: false,
@@ -44,19 +79,17 @@ const reducer = (state: any, action: any) => {
 
 const Icon: React.FC<IconProperty> = ({ style, id, name, top, left, img, onDoubleClick }) => {
   const [state, dispatch] = useReducer(reducer, {
-    mouse: { x: 0, y: 0 },
+    x: 0,
+    y: 0,
     top,
     left,
     selected: false,
+    dragging: false,
   })
 
   useEffect(() => {
     const onmousemove = (e: MouseEvent) => {
-      dispatch({
-        type: 'drag_dialog',
-        x: e.clientX,
-        y: e.clientY,
-      })
+      dispatch(drag(e.clientX, e.clientY))
     }
     document.addEventListener('mousemove', onmousemove)
     return () => {
@@ -65,10 +98,10 @@ const Icon: React.FC<IconProperty> = ({ style, id, name, top, left, img, onDoubl
   }, [])
   const onMouseDown = useCallback((e: React.MouseEvent) => {
     e.preventDefault();
-    dispatch({type: 'drag_start', x: e.clientX, y: e.clientY})
+    dispatch(dragStart(e.clientX, e.clientY))
   }, [])
   const onMouseUp = useCallback((e: React.MouseEvent) => {
-    dispatch({type: 'mouseup'})
+    dispatch(mouseup())
   }, [])
 
   return (
--- src/SObjectList.tsx
+++ src/SObjectList.tsx
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import Table from './Table';
 
-const jsforce = require('jsforce');
-
 interface SObjectListProperty {
   connection: any
 }
 
+interface DescribeResponse {
+  sobjects: any[]
+}
+
 const SObjectList: React.FC<SObjectListProperty> = ({ children, connection }) => {
-  const [sObjects, setSObjects] = useState([]);
+  const [sObjects, setSObjects] = useState([] as any[]);
   const [fields, setFields] = useState(['label', 'name'])
   const [isRequiredConnection, setIsRequiredConnection] = useState(false)
   
   useEffect(() => {
     if (connection) {
-      connection.describeGlobal((err: any, res: any) => {
+      connection.describeGlobal((err: string, res: DescribeResponse) => {
         if (err) { console.error(err); return; }
         setSObjects(res.sobjects.filter((r: any) => r.custom));
       });
